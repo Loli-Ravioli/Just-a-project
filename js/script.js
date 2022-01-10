a9dd58670ef59b70fecf3c893c2b7b598fd27819
Loli-Ravioli <81684400+Loli-Ravioli@users.noreply.github.com>
2022-01-10 11:51:34 +0300

    test

---
+++ js/script.js
@@ -0,0 +1,35 @@
+window.addEventListener("DOMContentLoaded",()=>{
+	const tabs = document.querySelectorAll(".tabheader__item"),
+		tabscontent = document.querySelectorAll(".tabcontent"),
+		tabparent = document.querySelector(".tabheader__items");
+
+	function hidetabscontent(){
+		tabscontent.forEach(element=>{
+			element.style.display="none";
+		})
+
+		tabs.forEach(element=>{
+			element.classList.remove("tabheader__item_active");
+		});
+	};
+	function showtabscontent(i=0) {
+		tabscontent[i].style.display="block";
+		tabs[i].classList.add(".tabheader__item_active")
+	}
+	hidetabscontent();
+	showtabscontent();
+
+	tabparent.addEventListener("click",(event)=>{
+		const target = event.target;
+
+		if (target && target.classList.contains("tabheader__item")){
+			 tabs.forEach((element,index)=>{
+			 	if(element==target){
+			 		hidetabscontent();
+					showtabscontent(index);
+			 	}
+			 })
+		}
+
+	})
+});
